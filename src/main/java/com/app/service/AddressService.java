bfb41fff2576d4c631bfa5d82fb6091bd445a391
Alok4690 <singhalok469@gmail.com>
2022-09-29 23:58:04 +0530

    address service implement

--- src/main/java/com/app/service/AddressService.java
+++ src/main/java/com/app/service/AddressService.java
@@ -1,7 +1,16 @@
 package com.app.service;
 
+import java.util.List;
+
+import com.app.exceptions.AddressException;
+import com.app.model.Address;
+
 public interface AddressService {
 
-	
+	public Address addAddress(Address add)throws AddressException;
+	public Address updateAddress(Address add)throws AddressException;
+	public Address removeAddress(Address add)throws AddressException;
+	public List<Address> viewAllAddress()throws AddressException;
+	public Address viewAddress(Integer id)throws AddressException;
 	
 }
