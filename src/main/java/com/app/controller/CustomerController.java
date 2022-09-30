22a673ba24d2a3e4f2cba351b4b4ae8774707ec1
BaluSaikumar <balusaikumar1999@gmail.com>
2022-09-30 20:29:51 +0530

    customer controller implimented successfully

--- src/main/java/com/app/controller/CustomerController.java
+++ src/main/java/com/app/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.app.controller;
 
 import java.util.List;
 
+import javax.security.auth.login.LoginException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.app.exceptions.CustomerException;
 import com.app.model.Customer;
 import com.app.service.CustomerService;
 
@@ -21,45 +24,45 @@ public class CustomerController {
 	private CustomerService customerService;
 	
 	@PostMapping("/addcustomer")
-	public ResponseEntity<Customer> addCustomerHandler(@RequestBody Customer customer){
+	public ResponseEntity<Customer> addCustomerHandler(@RequestBody Customer customer) throws CustomerException{
 		
-		Customer addedCustomer = null;
+		Customer addedCustomer = customerService.addCustomer(customer);
 		
 		 return new ResponseEntity<Customer>(addedCustomer, HttpStatus.CREATED);
 		
 	}
 	
-	@PutMapping("/updatecustomer")
-	public ResponseEntity<Customer> updateCustomerHandler(@RequestBody Customer customer){
+	@PutMapping("/updatecustomer/{key}")
+	public ResponseEntity<Customer> updateCustomerHandler(@PathVariable("key") String key,@RequestBody Customer customer) throws LoginException, CustomerException{
 		
-		Customer updatedCustomer = null;
+		Customer updatedCustomer = customerService.updateCustomer(customer, key);
 		
 		return new ResponseEntity<Customer>(updatedCustomer, HttpStatus.CREATED);
 		
 	}
 	
-	@DeleteMapping("/deletecustomer")
-	public ResponseEntity<Customer> removeCustomerHandler(@RequestBody Customer customer){
+	@DeleteMapping("/deletecustomer/{key}")
+	public ResponseEntity<Customer> removeCustomerHandler(@PathVariable("key") String key,@RequestBody Customer customer) throws CustomerException{
 		
-		Customer deletedCustomer = null;
+		Customer deletedCustomer = customerService.removeCustomer(customer, key);
 		
 		return new ResponseEntity<Customer>(deletedCustomer, HttpStatus.OK);
 		
 	}
 	
 	@GetMapping("/getcustomer")
-	public ResponseEntity<Customer> getCustomerHandler(@RequestBody Customer customer){
+	public ResponseEntity<Customer> getCustomerHandler(@RequestBody Customer customer) throws CustomerException{
 		
-		Customer existingCustomer = null;
+		Customer existingCustomer = customerService.viewCustomer(customer);
 		
 		return new ResponseEntity<Customer>(existingCustomer, HttpStatus.OK);
 		
 	}
 	
 	@GetMapping("/getallcustomers/{state}")
-	public ResponseEntity<List<Customer>> getAllCustomersByLocation(@PathVariable("state") String state){
+	public ResponseEntity<List<Customer>> getAllCustomersByLocation(@PathVariable("state") String state) throws CustomerException{
 		
-		List<Customer> customersByLocation = null;
+		List<Customer> customersByLocation = customerService.viewAllCustomer(state);
 		
 		return new ResponseEntity<List<Customer>>(customersByLocation, HttpStatus.OK);
 		
